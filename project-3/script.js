c31a258210f01e4998e1207841bea930157fa30f
Humfer <jcwong@usc.edu>
2022-05-05 14:01:15 -0700

    shadow

--- project-3/script.js
+++ project-3/script.js
@@ -54,12 +54,12 @@ function closeNav() {
 }
 
 function update(e){
-  var x = e.clientX || e.touches[0].clientX
-  var y = e.clientY || e.touches[0].clientY
+  var x = e.clientX || e.touches[0].clientX;
+  var y = e.clientY || e.touches[0].clientY;
 
-  document.documentElement.style.setProperty('--cursorX', x + 'px')
-  document.documentElement.style.setProperty('--cursorY', y + 'px')
+  document.documentElement.style.setProperty('--cursorX', x + 'px');
+  document.documentElement.style.setProperty('--cursorY', y + 'px');
 }
 
-document.addEventListener('mousemove',update)
-document.addEventListener('touchmove',update)
+document.addEventListener('mousemove',update);
+document.addEventListener('touchmove',update);
